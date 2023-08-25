2a275b6fca20b8b167e5968ae594420996c100f0
iLGZH <dearmengqiyuan@163.com>
2023-08-25 21:38:28 +0800

    add CPP:sfo_09

---
+++ CPP/include/include.hpp
@@ -0,0 +1,24 @@
+/*
+ * File: PrintUtil.hpp
+ * Created Time: 2021-12-19
+ * Author: Krahets (krahets@163.com)
+ */
+
+#pragma once
+
+#include <climits>
+#include <deque>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "ListNode.hpp"
+#include "PrintUtil.hpp"
+#include "TreeNode.hpp"
+
+using namespace std;
--- CPP/sfo_09_implement_queue_using_two_stacks_s1/sfo_09_implement_queue_using_two_stacks_s1.cc
+++ CPP/sfo_09_implement_queue_using_two_stacks_s1/sfo_09_implement_queue_using_two_stacks_s1.cc
@@ -1,10 +1,52 @@
-#include <stdio.h>
+#include "../include/include.hpp"
+#include <stack>
 
-int main( ) {
-    printf( "hello, world!" );
-    printf( "hello, world!" );
-    printf( "hello, world!" );
-    printf( "hello, world!" );
-    printf( "hello, world!" );
-    printf( "hello, world!" );
+class CQueue {
+public:
+    stack<int> s1, s2;
+    CQueue() {}
+
+    void appendTail( int value ) {
+        s1.push( value );
+    }
+
+    int deleteHead() {
+        if ( !s2.empty() ) {
+            int ans = s2.top();
+            s2.pop();
+            return ans;
+        }
+
+        if ( s1.empty() ) { return -1; }
+
+        while ( !s1.empty() ) {
+            s2.push( s1.top() );
+            s1.pop();
+        }
+
+        int ans = s2.top();
+        s2.pop();
+        return ans;
+    }
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue* obj = new CQueue();
+ * obj->appendTail(value);
+ * int param_2 = obj->deleteHead();
+ */
+
+int main() {
+    // ====== Driver Code ======
+    vector<int> res;
+    CQueue*     cQueue = new CQueue();
+    res.push_back( cQueue->deleteHead() );
+    cQueue->appendTail( 5 );
+    cQueue->appendTail( 2 );
+    res.push_back( cQueue->deleteHead() );
+    res.push_back( cQueue->deleteHead() );
+    PrintUtil::printVector( res );
+
+    return 0;
 }
